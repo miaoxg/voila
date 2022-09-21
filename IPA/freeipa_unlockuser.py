a6261fef707a88d3404438d9908e44af0140a6c5
晓光 妙 <miaoxiaoguang@voiladev.xyz>
2022-09-21 23:28:31 +0800

    add freeipa_unlockuser.py

---
+++ IPA/freeipa_unlockuser.py
@@ -0,0 +1,14 @@
+from python_freeipa import ClientMeta
+
+client = ClientMeta('ipa.voiladev.xyz', verify_ssl=False)
+client.login('miaoxiaoguang', 'M$@qenZ4#jzwC6gx')
+
+
+def user_unlock():
+    email_address = input("输入待解锁的邮箱")
+    user = email_address.split('@')[0]
+    client.user_unlock(user)
+
+
+if __name__ == '__main__':
+    user_unlock()
