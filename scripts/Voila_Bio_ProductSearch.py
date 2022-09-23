7d563b738ed9228238656cda0e10e03a91f6c4c1
晓光 妙 <miaoxiaoguang@voiladev.xyz>
2022-09-24 00:33:04 +0800

    add logging module,fix cookies expiration

--- scripts/Voila_Bio_ProductSearch.py
+++ scripts/Voila_Bio_ProductSearch.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import json
+import logging
+import platform
 import random
 import re
+import threading
 import time
 
 import requests
@@ -19,28 +22,21 @@ USERNAME = 'xiao20090813xiao@163.com'
 PASSWORD = 'sunsh1ne0sunny'
 # the keyword for searching product
 PRODUCTWORD = ['red', 'red shirt', 'man', 'child']
-# total_statuscode_list = []
-# total_title_list = []
+
 requests_cookies = {}
 
-seconds = random.randint(1, 2)
-chrome_options = Options()
-# 不加载ui
-chrome_options.add_argument("--headless")
-chrome_options.add_argument('--disable-blink-features=AutomationControlled')  ## to avoid getting detected
-driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
-# 可控制窗口大小
-# driver.maximize_window()
-wait = WebDriverWait(driver, 10)
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s %(filename)s %(funcName)s：line %(lineno)d threadid %(thread)d %(levelname)s %(message)s",
+                    datefmt='%Y-%m-%d %H:%M:%S'
+                    )
 
 
-# define function for pushing alerts to prometheus
-def pushalert(status="-1"):
+def pushalert(metric_name="test", metric_value="-1", job_name="job_name"):
     result = client.push_data(
         url="pushgateway.voiladev.xyz:32684",
-        metric_name="creator_search_product_status",
-        metric_value=status,
-        job_name="creator_search_product",
+        metric_name=metric_name,
+        metric_value=metric_value,
+        job_name=job_name,
         timeout=5,
         labels={
             "env": "prod"
@@ -49,113 +45,133 @@ def pushalert(status="-1"):
 
 
 def login_get_cookies():
-    # load page
-    try:
-        driver.get('https://creator.voila.love')
-    except Exception as e:
-        pushalert("4")
-        # exit()
-    # 等待页面加载
-    time.sleep(seconds)
+    while True:
+        driver = ""
+        if driver:
+            driver.quit()
 
-    try:
-        wait.until(EC.presence_of_element_located(
-            (By.XPATH, "//*[@id=\"app\"]/div/div[2]/div[1]/div[2]/form/div[1]/div/div[1]/input"))).send_keys(USERNAME)
-    except Exception as e:
-        pushalert("6")
-        # exit()
-    try:
-        wait.until(EC.presence_of_element_located(
-            (By.XPATH, "/html/body/div/div/div[2]/div[1]/div[2]/form/div[2]/div/div[1]/input"))).send_keys(PASSWORD)
-    except Exception as e:
-        pushalert("7")
-        # exit()
-    # click "SIGN IN" button
-    try:
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                               '#app > div > div.container__main > div.login > div.login-form > form > div:nth-child(3) > div > button'))).click()
-    except Exception as e:
-        pushalert("1")
-        # exit()
+        seconds = random.randint(5, 9)
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument('--disable-blink-features=AutomationControlled')  ## to avoid getting detected
 
-    # sleep必须要有，否则cookies获取不全
-    time.sleep(seconds)
+        if platform.system() == "Linux":
+            chrome_options.add_argument('--remote-debugging-port=9225')
+            driver = webdriver.Chrome(options=chrome_options, executable_path='/usr/bin/chromedriver')
+        else:
+            driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
+        wait = WebDriverWait(driver, 10)
+        logging.info("begin get cookies")
+        # load page
+        try:
+            driver.get('https://creator.voila.love')
+        except Exception as e:
+            pushalert("voila_searchproduct_status", "1", "voila_searchproduct")
+            # exit()
+        # 等待页面加载
+        time.sleep(seconds)
 
-    cookies = driver.get_cookies()
-    for c in cookies:
-        requests_cookies[c['name']] = c['value']
+        try:
+            wait.until(EC.presence_of_element_located(
+                (By.XPATH, "//*[@id=\"app\"]/div/div[2]/div[1]/div[2]/form/div[1]/div/div[1]/input"))).send_keys(
+                USERNAME)
+        except Exception as e:
+            pushalert("voila_searchproduct_status", "2", "voila_searchproduct")
+            # exit()
+        try:
+            wait.until(EC.presence_of_element_located(
+                (By.XPATH, "/html/body/div/div/div[2]/div[1]/div[2]/form/div[2]/div/div[1]/input"))).send_keys(PASSWORD)
+        except Exception as e:
+            pushalert("voila_searchproduct_status", "3", "voila_searchproduct")
+            # exit()
+        # click "SIGN IN" button
+        try:
+            wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                   '#app > div > div.container__main > div.login > div.login-form > form > div:nth-child(3) > div > button'))).click()
+        except Exception as e:
+            pushalert("voila_searchproduct_status", "4", "voila_searchproduct")
+            # exit()
 
-    if requests_cookies:
-        return True
-    else:
-        # status=2 get cookies failed
-        pushalert("2")
-    driver.close()
+        # sleep必须要有，否则cookies获取不全
+        time.sleep(seconds)
 
+        cookies = driver.get_cookies()
+        for c in cookies:
+            requests_cookies[c['name']] = c['value']
 
-def search_product(query_word="blue"):
-    headers = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-        "sec-ch-ua-platform": "macOS",
-        "content-type": "application/json"
-    }
+        if requests_cookies:
+            logging.info('Generate cookies successfully!')
+        else:
+            # status=5 get cookies failed
+            pushalert("voila_searchproduct_status", "5", "voila_searchproduct")
+            logging.info("Generate cookies failed!")
 
-    data = {
-        "query": query_word,
-        "sort": "SortTypeDefault",
-        "page": 1,
-        "pageSize": 10,
-        "cursor": "",
-        "isCollect": "false",
-        "attributes": [],
-        "categoryIds": [],
-        "brands": [],
-        "retailers": [],
-        "price": {"min": 0, "max": 0},
-        "discount": []
-    }
-
-    try:
-        response = requests.post('https://creator.voila.love/_/voila/v2/product-gateway/search',
-                                 cookies=requests_cookies, json=data, headers=headers)
-        response_data = json.loads(response.text)
-        title_list = re.findall('title\': [\'|\"](.+?)[\'|\"],', str(response_data))
-        for i in title_list:
-            total_title_list.append(i)
-        total_statuscode_list.append(response.status_code)
-    except Exception as e:
-        pushalert("5")
-
-    # if response.status_code != 200:
-    #     pushalert("5")
-    # exit()
-    # response_data = json.loads(response.text)
-
-    # title_list = re.findall('title\': \'(.+?)\',',str(response_data))
-    # title_list = re.findall('title\': [\'|\"](.+?)[\'|\"],',str(response_data))
-    # print(query_word,title_list)
-
-    # for i in title_list:
-    #     total_title_list.append(i)
-    # # for j in list(str(response.status_code)):
-    # total_statuscode_list.append(response.status_code)
+    time.sleep(86400 * 6)
 
 
-if __name__ == "__main__":
-    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "Begin#" * 10)
-    login_get_cookies()
+def search_product():
     while True:
         total_statuscode_list = []
         total_title_list = []
+
+        if not requests_cookies:
+            time.sleep(30)
         for i in PRODUCTWORD:
-            search_product(i)
-        print("total_statuscode_list is:", total_statuscode_list)
-        print("total_title_list is:", total_title_list)
-        print("len(total_statuscode_list) is:", len(total_statuscode_list))
-        print("len(total_title_list) is:", len(total_title_list))
+            headers = {
+                "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+                "sec-ch-ua-platform": "macOS",
+                "content-type": "application/json"
+            }
+
+            data = {
+                "query": i,
+                "sort": "SortTypeDefault",
+                "page": 1,
+                "pageSize": 10,
+                "cursor": "",
+                "isCollect": "false",
+                "attributes": [],
+                "categoryIds": [],
+                "brands": [],
+                "retailers": [],
+                "price": {"min": 0, "max": 0},
+                "discount": []
+            }
+
+            try:
+                response = requests.post('https://creator.voila.love/_/voila/v2/product-gateway/search',
+                                         cookies=requests_cookies, json=data, headers=headers)
+                logging.info('query word is %s', i)
+                response_data = json.loads(response.text)
+                title_list = re.findall('title\': [\'|\"](.+?)[\'|\"],', str(response_data))
+                for j in title_list:
+                    total_title_list.append(j)
+                total_statuscode_list.append(response.status_code)
+                logging.info('total_statuscode_list is %s', total_statuscode_list)
+            except Exception as e:
+                pushalert("voila_searchproduct_status", "6", "voila_searchproduct")
+                logging.info("response.status_code is %s", response.status_code)
+            time.sleep(12)
+
+        logging.info("total_statuscode_list is: %s", total_statuscode_list)
+        logging.info("total_title_list is: %s", total_title_list)
+        logging.info("len(total_statuscode_list) is: %s", len(total_statuscode_list))
+        logging.info("len(total_title_list) is: %s", len(total_title_list))
+
         if total_statuscode_list.count(200) == len(PRODUCTWORD) and len(total_title_list) >= (
-                len(PRODUCTWORD) - 1) * 10 and len(PRODUCTWORD) <= len(PRODUCTWORD) * 10:
-            pushalert("0")
+                len(PRODUCTWORD) - 1) * 10:
+            pushalert("voila_searchproduct_status", "0", "voila_searchproduct")
+            logging.info("search products successfully")
         else:
-            pushalert("3")
-        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "end#" * 10)
+            pushalert("voila_searchproduct_status", "7", "voila_searchproduct")
+            logging.info("search products failed")
+
+
+if __name__ == "__main__":
+    p1 = threading.Thread(target=login_get_cookies)
+    p2 = threading.Thread(target=search_product)
+    p1.start()
+    p2.start()
