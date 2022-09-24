31c857fdb119031c263846ff57fb4c76def4422b
晓光 妙 <miaoxiaoguang@voiladev.xyz>
2022-09-24 14:31:37 +0800

    modify usebouncer_check.py time.sleep()

--- scripts/usebouncer_check.py
+++ scripts/usebouncer_check.py
@@ -30,13 +30,15 @@ def useboncer_check():
 
         header = {
             'authority': 'api.usebouncer.com',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;'
+                      'q=0.8,application/signed-exchange;v=b3;q=0.9',
             'accept-language': 'en,zh;q=0.9,zh-CN;q=0.8',
             'cache-control': 'no-cache',
             'pragma': 'no-cache',
             "sec-ch-ua-platform": "macOS",
             'sec-fetch-dest': 'document',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
             'x-api-key': '11rVwtTjIoCOk9wxXd9sPyMsqpUGRCWVhGSKZm8S'
         }
 
